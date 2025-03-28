59fa8176bc4f26bd1ea79263a3a0ede4a2239b87
phamthithuha3112@gmail.com <phamthithuha3112@gmail.com>
2025-03-28 16:16:17 +0700

    home

--- src/components/Client/home-page/index.vue
+++ src/components/Client/home-page/index.vue
@@ -1,13 +1,39 @@
-<template lang="">
-    <div>
-        asdfgh
+<template>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="">
+                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+            </div>
+            <h5>ĐƯỢC TIN DÙNG BỞI HÀNG NGÀN NGƯỜI YÊU THÚ CƯNG</h5>
+            <h1>Chăm sóc thú cưng tận tâm</h1>
+            <p>Hộp <strong>PetCare Box</strong> của chúng tôi là gói đăng ký hàng tháng bao gồm:</p>
+            <ul>
+                <li>✔ Đồ chơi bền, tương tác giúp thú cưng vui vẻ 🧸</li>
+                <li>✔ Thức ăn vặt bổ dưỡng, an toàn 🥩</li>
+                <li>✔ Quà tặng đặc biệt dành riêng cho bé cưng 🎁</li>
+                <li>✔ Quà tặng đặc biệt dành riêng cho bé cưng 🎁</li>
+                <li>✔ Quà tặng đặc biệt dành riêng cho bé cưng 🎁</li>
+                <li>✔ Quà tặng đặc biệt dành riêng cho bé cưng 🎁</li>
+                <li>✔ Quà tặng đặc biệt dành riêng cho bé cưng 🎁</li>
+            </ul>
+            <p>✨ Mang đến niềm vui và sự chăm sóc tốt nhất cho thú cưng của bạn!</p>
+            <button class="btn btn-primary"><a href="#" class="btn">Nhận ngay PetCare Box</a></button>
+            
+        </div>
+        <div class="col-lg-6">
+            <video class="rounded" src="https://cdn.shopify.com/videos/c/o/v/1c2a100d09ea40cfa768b9b749e8fcb5.mp4"
+                style="width:100%;height:100%;" preload="metadata" autoplay="" muted="true" loop=""></video>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
-<style lang="">
-    
+<style>
 </style>
