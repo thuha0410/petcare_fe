336bdc579d2b53f78fe6489626b9156abb4e424f
Thao <nguyenthithuthao452003@gmail.com>
2025-04-08 15:30:45 +0700

    luu

--- src/components/Client/BangGia/index.vue
+++ src/components/Client/BangGia/index.vue
@@ -2,6 +2,25 @@
     <div class="row">
         <div class="col-lg-2"></div>
         <div class="col-lg-8 ">
+            <div class="table table-responsive">
+                <table class="table table-bordered">
+                <thead>
+                    <tr class="bg-info" style="color: darkblue;">
+                        <th class="text-center aligh-middle">Dịch vụ</th>
+                        <th class="text-center aligh-middle">Đối tượng</th>
+                        <th class="text-center aligh-middle">Giá</th>
+                    </tr>
+                    <tr class="bg-">
+                        <th>GÓI CẮT TỈA CƠ BẢN</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
             <img class="img-fluid" src="https://res.cloudinary.com/dd1p908gm/image/upload/v1743755986/The_Art_of_Living_wm7rbb.jpg"
                 alt="">
         </div>
