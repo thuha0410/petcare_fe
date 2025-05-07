1f59141aefd34fd541be52a62d87ce8f4a08c5ba
doancarat <aligyokugaming@gmail.com>
2025-05-07 14:01:35 +0700

    aaa

--- src/components/Admin/QLLuong/index.vue
+++ src/components/Admin/QLLuong/index.vue
@@ -8,8 +8,6 @@
                 <div class="card-body">
                     <div class="input-group">
                     </div>
-                    <label for="">Mã lương</label>
-                    <input v-model="loai_luong.id_luong" class="form-control" type="text">
                     <label for="">Tên nhân viên</label>
                     <select v-model="loai_luong.id_nv" class="form-control">
                         <template v-for="(value, index) in nhan_vien" :key="index">
@@ -51,7 +49,6 @@
                             <thead>
                                 <tr>
                                     <th class="text-center align-middle">#</th>
-                                    <th class="text-center align-middle">Mã lương</th>
                                     <th class="text-center align-middle">Tên nhân viên</th>
                                     <th class="text-center align-middle">Tiền lương</th>
                                     <th class="text-center align-middle">Ngày thanh toán</th>
@@ -64,7 +61,6 @@
                                 <template v-for="(value, index) in ds_luong" :key="index">
                                     <tr>
                                         <td class="text-center align-middle">{{ index + 1 }}</td>
-                                        <td class="text-center align-middle">{{ value.id_luong }}</td>
                                         <td class="text-center align-middle">{{ value.ten_nv }}</td>
                                         <td class="text-center align-middle">{{ value.tien_luong }}</td>
                                         <td class="text-center align-middle">{{ value.ngay_thanh_toan }}</td>
@@ -100,8 +96,6 @@
                     <h1 class="modal-title fs-5 text-white " id="exampleModalLabel">SỬA THÔNG TIN LƯƠNG</h1>
                 </div>
                 <div class="modal-body">
-                    <label for="">Mã lương</label>
-                    <input v-model="sua_luongNV.id_luong" class="form-control" type="text">
                     <label for="">Tên nhân viên</label>
                     <select v-model="sua_luongNV.id_nv" class="form-control">
                         <template v-for="(value, index) in nhan_vien" :key="index">
@@ -114,7 +108,6 @@
                     <input v-model="sua_luongNV.ngay_thanh_toan" class="form-control" type="date">
                     <label for="">Tiền thưởng</label>
                     <input v-model="sua_luongNV.tien_thuong" class="form-control" type="text">
-
                 </div>
                 <div class="modal-footer">
                     <button v-on:click="sua()" type="button" class="btn btn-primary" data-bs-dismiss="modal">Cập
@@ -153,7 +146,6 @@ export default {
             ds_luong: [],
             nhan_vien: [],
             loai_luong: {
-                'id_luong': "",
                 'id_nv': "",
                 'tien_luong': "",
                 'ngay_thanh_toan': "",
@@ -249,7 +241,13 @@ export default {
         },
         loadDataNV() {
             axios
-                .get('http://127.0.0.1:8000/api/nhan-vien/load')
+                .get('http://127.0.0.1:8000/api/nhan-vien/load',
+                {
+                        headers: {
+                            Authorization: 'Bearer ' + localStorage.getItem('token_admin')
+                        }
+                    }
+                )   
                 .then(
                     (res) => {
                         this.nhan_vien = res.data.data;
