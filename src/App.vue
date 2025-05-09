38155a14a3bd4dce0d51c10b305d3dbb89e294e3
suyg3tsu <145313056+suyg3tsu@users.noreply.github.com>
2025-05-09 18:51:14 +0700

    chatbot

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <component :is="layout">
       <router-view></router-view>
+      <Chatbot />
   </component>
 </template>
 
 <script>
+import Chatbot from './components/Chatbot.vue'
+
 const default_layout = "default";
 export default {
+  name: 'App',
+  components: {
+    Chatbot
+  },
   computed: {
     layout() {
       return (this.$route.meta.layout || default_layout) + "-layout";
